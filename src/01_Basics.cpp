b9dad94ad8513041e4f343a7cbeca0738ff6f4b4
imasifuddin <imasifuddin@gmail.com>
2026-01-11 23:31:01 +0530

    first commit

---
+++ src/01_Basics.cpp
@@ -0,0 +1,10 @@
+#include<iostream> //specific header files
+#include<bits/stdc++.h> // for all c++ header files
+
+using namespace std;
+
+int main() {
+
+    cout << "hello asif";
+    return 0;
+}
